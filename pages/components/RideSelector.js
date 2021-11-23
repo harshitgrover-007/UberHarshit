9b02cae5c8987a0b28821ff3a3a767fdda35ced2
harshitgrover-007 <74338878+harshitgrover-007@users.noreply.github.com>
2021-11-23 02:45:21 -0800

    Update RideSelector.js

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import tw from "tailwind-styled-components/dist/tailwind";
-import { carList } from "../carList";
+import { CArList } from "../carList";
 
 import Link from "next/link";
 
@@ -22,7 +22,7 @@ const RideSelector = ({ pickCoordinates, dropCoordinates }) => {
     <Wrapper>
       <Titl>Choose a ride, or swipe up for more</Titl>
       <CarList>
-        {carList.map((car, index) => (
+        {CArList.map((car, index) => (
           <Car key={index}>
             
               <CarImage src={car.imgUrl} />
